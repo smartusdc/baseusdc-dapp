80e75d7f9043e3048010b520e841afd8e3f87b89
smartusdc <jakepaul900009@gmail.com>
2025-01-18 07:06:34 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, BASEUSDC.COM!</h1>
+    <div className="container">
+      <div className="header">
+        <div className="logo-container">
+          <img src="/logo.png" alt="BASEUSDC.COM" className="logo" />
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <button id="connectWallet" className="button">Connect Wallet</button>
+        </div>
+      </div>
+
+      {/* 既存のHTMLをここに移行 */}
     </div>
   );
 }
